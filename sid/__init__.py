5613e5aa985e4e43e53be0d8365a99c1554f6b9a
Dmitrii Polikarpov <dmitry1.1.1900@gmail.com>
2020-07-01 10:45:00 +0300

    The flask application was created. The folder "templates" with the file "index.html" were created.

---
+++ sid/__init__.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route('/')
+    def index():
+        title = "Smart invest decision"
+        return render_template('index.html', page_title=title)
+    return app
+
