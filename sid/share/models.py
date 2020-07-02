899d1928922b1b3e2bab6f0fd364c50fe2f128ae
Dmitrii Polikarpov <dmitry1.1.1900@gmail.com>
2020-07-02 22:33:41 +0300

    Columns were changed. Low, high, open and close prices were added. Volume was added.

--- sid/share/models.py
+++ sid/share/models.py
@@ -5,9 +5,14 @@ class Share(db.Model):
     """the model to save data of the shares of different companies"""
     id = db.Column(db.BigInteger, primary_key=True)
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
-    date = db.Column(db.Date, nullable=False)
-    price = db.Column(db.Numeric, nullable=False)
-    lot = db.Column(db.BigInteger, nullable=False)
+    trading_date = db.Column(db.Date, nullable=False)
+    open_price = db.Column(db.Numeric, nullable=False)
+    high_price = db.Column(db.Numeric, nullable=False)
+    low_price = db.Column(db.Numeric, nullable=False)
+    close_price = db.Column(db.Numeric, nullable=False)
+    volume = db.Column(db.Numeric, nullable=False)
+    dividends = db.Column(db.Numeric, nullable=True)
+    stock_split = db.Column(db.Numeric, nullable=True)
 
     def __repr__(self):
         return f'Share {self.name}'
