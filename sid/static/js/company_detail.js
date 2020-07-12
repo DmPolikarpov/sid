358c9175f84f0b933733f0911ac67fcab3abe784
Dmitrii Polikarpov <dmitry1.1.1900@gmail.com>
2020-07-12 10:08:45 +0300

    the files company_detail.css and company_detail.js were created. Company/detail.html was modified (the title and tabs were added)

---
+++ sid/static/js/company_detail.js
@@ -0,0 +1,14 @@
+/*hide the content of the tabs, activate the first tab and show the first tab's content*/
+$(document).ready(function() {
+    $("#content div").hide();
+    $("#tabs li:first").attr("id","current");
+    $("#content div:first").fadeIn();
+/*processing a tab click action*/
+    $('#tabs a').click(function(e) {
+        e.preventDefault();
+        $("#content div").hide();
+        $("#tabs li").attr("id","");
+        $(this).parent().attr("id","current");
+        $('#' + $(this).attr('title')).fadeIn();
+    });
+});
